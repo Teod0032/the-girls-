45a6b15cbb5ee5100639f75e9a9f59a070194074
Freja-absalonsen <freja.absalonsen.laursen@gmail.com>
2025-09-02 10:17:07 +0200

    Create skift.js

---
+++ skift.js
@@ -0,0 +1,10 @@
+const variantData = {
+    sort:{
+        name: "Limona Top • sort "
+        priceNow: 359.40,
+        priceBefore: 599.00,
+        images: ["img/top-black-1.jpg","img/top-black-2.jpg","img/top-black-3.jpg","img/top-black-4.jpg"]
+        },
+
+    }
+}
